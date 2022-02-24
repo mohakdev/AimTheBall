414f77d5405fb5a2e742e35e3ef4ec2f2ee89870
Mohak Jain <mohakjain55055@gmail.com>
2022-02-24 21:02:29 +0530

    Retry and Level Cleared UI made

--- AimTheBall/Assets/Scripts/LevelClearedUI.cs
+++ AimTheBall/Assets/Scripts/LevelClearedUI.cs
@@ -1,18 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class LevelClearedUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void NextLevel()
     {
-        
+        //Load Next level here
     }
-
-    // Update is called once per frame
-    void Update()
+    public void BackToMenu()
     {
-        
+        SceneManager.LoadScene("Main Menu");
     }
 }
--- AimTheBall/Assets/Scripts/LevelFailedUI.cs
+++ AimTheBall/Assets/Scripts/LevelFailedUI.cs
@@ -1,18 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class LevelFailedUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void RetryFunction()
     {
-        
+        SceneManager.LoadScene("Gameplay");
     }
-
-    // Update is called once per frame
-    void Update()
+    public void BackToMenu()
     {
-        
+        SceneManager.LoadScene("Main Menu");
     }
 }
