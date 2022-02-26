cd61115dc0ae0c44c7807423b4a7ee2a1e654790
Mohak Jain <mohakjain55055@gmail.com>
2022-02-26 11:22:22 +0530

    Level Selection Work Started

--- AimTheBall/Assets/Scripts/LevelSelect.cs
+++ AimTheBall/Assets/Scripts/LevelSelect.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class LevelSelect : MonoBehaviour
 {
-    // Start is called before the first frame update
+    int LevelUnlocked;
+    public int TotalLevels;
+    public GameObject LevelButton;
+    public GameObject LevelGrid;
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        int i = 1;
+        LevelUnlocked = PlayerPrefs.GetInt("LevelUnlocked", 1);
+        while (i <= TotalLevels)
+        {
+            //Getting all important things
+            GameObject ButtonObject = Instantiate(LevelButton);
+            Button ButtonComponent = ButtonObject.GetComponent<Button>();
+            GameObject TextObject = ButtonObject.transform.Find("Text").gameObject;
+            Text ButtonText = TextObject.GetComponent<Text>();
+            GameObject LockIcon = ButtonObject.transform.Find("LockIcon").gameObject;
+            ButtonObject.transform.SetParent(LevelGrid.transform, false);
+            if (i <= LevelUnlocked)
+            {
+                ButtonText.text = i.ToString();
+            }
+            else
+            {
+                ButtonText.text = "";
+                ButtonComponent.interactable = false;
+                LockIcon.SetActive(true);
+            }
+            i++;
+        }
     }
 }
